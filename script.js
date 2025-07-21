1dc2ac8059d26cd76a6034a296962ea0c439de73
rosielockett95 <rosie.lockett4@gmail.com>
2025-07-21 16:03:56 +0100

    second version of project

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-const buttons = document.querySelectorAll("[data-carousel-button]");
+const carousels = document.querySelectorAll(".carousel");
+const hambugerIcon = document.querySelector(".hamburger");
+const hiddenNav = document.querySelector(".nav-bar-hidden");
+const logo = document.querySelector(".logo");
+const closeLogo = document.getElementById("close");
+const isDesktop = window.matchMedia("(max-width: 375px)");
+const carouselButton = document.querySelector(".grid-two-arrow-container");
+const carouselButtonTwo = document.querySelector(".grid-three-arrow-container");
+const carouselButtonThree = document.querySelector(
+  ".grid-four-arrow-container"
+);
+const gridOne = document.querySelector(".grid-one");
+const gridTwo = document.querySelector(".slide-two");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.buttons === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel")
-      .querySelector("[data-slides");
+if (carousels.length) {
+  carousels.forEach((carousel) => {
+    const carouselButtons = carousel.querySelectorAll(".carousel__button");
+    const carouselSlides = carousel.querySelector(".carousel__slides");
+    const currentSlide = carousel.querySelector(".slide");
+    const currentSlideWidth = currentSlide.getBoundingClientRect().width;
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+    if (carouselButtons.length) {
+      carouselButtons.forEach((button) =>
+        button.addEventListener("click", (e) => {
+          e.stopPropagation();
+          const button = e.target.closest("button");
+          if (!button) return;
+          if (button.dataset.button === "prev") {
+            carouselSlides.scrollTo({
+              left: carouselSlides.scrollLeft - currentSlideWidth,
+              behavior: "smooth",
+            });
+            console.log("prev");
+          } else {
+            carouselSlides.scrollTo({
+              left: carouselSlides.scrollLeft + currentSlideWidth,
+              behavior: "smooth",
+            });
+            console.log("next");
+          }
+        })
+      );
+    }
   });
-});
+}
+
+if (hambugerIcon) {
+  hambugerIcon.addEventListener("click", () => {
+    hiddenNav.classList.toggle("hidden");
+    hambugerIcon.classList.remove("hamburger");
+    logo.classList.toggle("hidden");
+  });
+}
+
+if (closeLogo) {
+  closeLogo.addEventListener("click", () => {
+    hiddenNav.classList.toggle("hidden");
+    hambugerIcon.classList.add("hamburger");
+    logo.classList.toggle("hidden");
+  });
+}
+
+if (isDesktop.matches) {
+  gridOne.append(carouselButton);
+}
