99afa0bc9dea64e2d65a36f3cbda55ad6c42426b
rosielockett95 <rosie.lockett4@gmail.com>
2025-07-23 16:34:51 +0100

    mobile nav complete

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
-const carousels = document.querySelectorAll(".carousel");
-const hambugerIcon = document.querySelector(".hamburger");
-const hiddenNav = document.querySelector(".nav-bar-hidden");
-const logo = document.querySelector(".logo");
-const closeLogo = document.getElementById("close");
-const isDesktop = window.matchMedia("(max-width: 375px)");
-const carouselButton = document.querySelector(".grid-two-arrow-container");
-const carouselButtonTwo = document.querySelector(".grid-three-arrow-container");
-const carouselButtonThree = document.querySelector(
-  ".grid-four-arrow-container"
-);
-const gridOne = document.querySelector(".grid-one");
-const gridTwo = document.querySelector(".slide-two");
+document.addEventListener("DOMContentLoaded", () => {
+  const carousels = document.querySelectorAll(".carousel");
+  const hambugerIcon = document.querySelector(".hamburger");
+  const hiddenNav = document.querySelector(".nav-bar-hover");
+  const logo = document.querySelector(".logo");
+  const closeLogo = document.getElementById("close");
+  const isDesktop = window.matchMedia("(max-width: 375px)");
+  const desktopNavBar = document.querySelector(".nav-bar--desktop");
+  const mobileNavBar = document.querySelector(".nav-bar--mobile ");
+  const carouselButtons = document.querySelector(".carousel__buttons");
+  const bannerLeft = document.querySelector(".banner__left");
 
-if (carousels.length) {
-  carousels.forEach((carousel) => {
-    const carouselButtons = carousel.querySelectorAll(".carousel__button");
-    const carouselSlides = carousel.querySelector(".carousel__slides");
-    const currentSlide = carousel.querySelector(".slide");
-    const currentSlideWidth = currentSlide.getBoundingClientRect().width;
-
-    if (carouselButtons.length) {
-      carouselButtons.forEach((button) =>
-        button.addEventListener("click", (e) => {
-          e.stopPropagation();
-          const button = e.target.closest("button");
-          if (!button) return;
-          if (button.dataset.button === "prev") {
-            carouselSlides.scrollTo({
-              left: carouselSlides.scrollLeft - currentSlideWidth,
-              behavior: "smooth",
-            });
-            console.log("prev");
-          } else {
-            carouselSlides.scrollTo({
-              left: carouselSlides.scrollLeft + currentSlideWidth,
-              behavior: "smooth",
-            });
-            console.log("next");
-          }
-        })
+  if (carousels.length) {
+    carousels.forEach((carousel) => {
+      const carouselID = carousel.id;
+      console.dir(carousel);
+      const carouselButtonContainer = document.querySelector(
+        `[data-carousel='${carouselID}']`
       );
-    }
-  });
-}
+      console.log(carouselButtonContainer);
+      const carouselSlides = carousel.querySelector(".carousel__slides");
+      const currentSlide = carousel.querySelector(".slide");
+      const currentSlideWidth = currentSlide.getBoundingClientRect().width;
+      if (carouselButtonContainer) {
+        const carouselButtons =
+          carouselButtonContainer.querySelectorAll("button");
+        if (carouselButtons.length) {
+          carouselButtons.forEach((button) =>
+            button.addEventListener("click", (e) => {
+              e.stopPropagation();
+              const button = e.target.closest("button");
+              if (!button) return;
+              if (button.dataset.button === "prev") {
+                carouselSlides.scrollTo({
+                  left: carouselSlides.scrollLeft - currentSlideWidth,
+                  behavior: "smooth",
+                });
+                console.log("prev");
+              } else {
+                carouselSlides.scrollTo({
+                  left: carouselSlides.scrollLeft + currentSlideWidth,
+                  behavior: "smooth",
+                });
+                console.log("next");
+              }
+            })
+          );
+        }
+      }
+    });
+  }
 
-if (hambugerIcon) {
-  hambugerIcon.addEventListener("click", () => {
-    hiddenNav.classList.toggle("hidden");
-    hambugerIcon.classList.remove("hamburger");
-    logo.classList.toggle("hidden");
-  });
-}
+  if (hambugerIcon) {
+    hambugerIcon.addEventListener("click", () => {
+      hiddenNav.classList.remove("hidden");
+      hiddenNav.classList.toggle("open");
+      hambugerIcon.classList.remove("hamburger");
+      logo.classList.toggle("hidden");
+    });
+  }
 
-if (closeLogo) {
-  closeLogo.addEventListener("click", () => {
-    hiddenNav.classList.toggle("hidden");
-    hambugerIcon.classList.add("hamburger");
-    logo.classList.toggle("hidden");
-  });
-}
+  if (closeLogo) {
+    closeLogo.addEventListener("click", () => {
+      hiddenNav.classList.toggle("open");
+      hambugerIcon.classList.add("hamburger");
+      logo.classList.toggle("hidden");
+    });
+  }
 
-if (isDesktop.matches) {
-  gridOne.append(carouselButton);
-}
+  if (isDesktop.matches) {
+    desktopNavBar.classList.add("hidden");
+    mobileNavBar.classList.remove("hidden");
+    bannerLeft.append(carouselButtons);
+  }
+});
